fc1b5c23401d57cbdf2288aa42a730c390a96914
Kyle K <kyliek24@hotmail.co.uk>
2011-12-19 23:41:09 +0000

    added easy keycode definition macro, added alphabetical keycodes, played with test.scm

--- sfml.scm
+++ sfml.scm
@@ -28,13 +28,22 @@
 
 	      sf-event-create
 	      sf-event-closed?
+
 	      sf-event-key-pressed?
           sf-event-key-ctrl?
           sf-event-key-alt?
           sf-event-key-system?
           sf-event-key-shift?
           sf-event-key?
-          sf-key-A
+
+          sf-key-A sf-key-B sf-key-C sf-key-D
+          sf-key-E sf-key-F sf-key-G sf-key-H
+          sf-key-I sf-key-J sf-key-K sf-key-L
+          sf-key-M sf-key-N sf-key-O sf-key-P
+          sf-key-Q sf-key-R sf-key-S sf-key-T
+          sf-key-U sf-key-V sf-key-W sf-key-X
+          sf-key-Y sf-key-Z
+
 	      )
   (import scheme chicken foreign)
 
@@ -157,8 +166,42 @@ EOF
   (not (zero? ((foreign-lambda* int (((c-pointer sfEvent) event))
      "return ((int)event->Key.System);") event))))
 
-(define sf-key-A
-  (foreign-value "sfKeyA" int))
+(define-syntax define-key
+  (syntax-rules ()
+    ((define-key sym name)
+     (define sym
+       (foreign-value name int)))))
+
+(define-key sf-key-A "sfKeyA")
+(define-key sf-key-B "sfKeyB")
+(define-key sf-key-C "sfKeyC")
+(define-key sf-key-D "sfKeyD")
+(define-key sf-key-E "sfKeyE")
+(define-key sf-key-F "sfKeyF")
+(define-key sf-key-G "sfKeyG")
+(define-key sf-key-H "sfKeyH")
+(define-key sf-key-I "sfKeyI")
+(define-key sf-key-J "sfKeyJ")
+(define-key sf-key-K "sfKeyK")
+(define-key sf-key-L "sfKeyL")
+(define-key sf-key-M "sfKeyM")
+(define-key sf-key-N "sfKeyN")
+(define-key sf-key-O "sfKeyO")
+(define-key sf-key-P "sfKeyP")
+(define-key sf-key-Q "sfKeyQ")
+(define-key sf-key-R "sfKeyR")
+(define-key sf-key-S "sfKeyS")
+(define-key sf-key-T "sfKeyT")
+(define-key sf-key-U "sfKeyU")
+(define-key sf-key-V "sfKeyV")
+(define-key sf-key-W "sfKeyW")
+(define-key sf-key-X "sfKeyX")
+(define-key sf-key-Y "sfKeyY")
+(define-key sf-key-Z "sfKeyZ")
+
+
+;(define sf-key-A
+ ; (foreign-value "sfKeyA" int))
 
 (define (sf-event-key? event key)
   (not (zero? ((foreign-lambda* int (((c-pointer sfEvent) event) (int key))
