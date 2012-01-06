732bd7961b45bd381ac2395f42b72f4e63746952
Kyle K <kyliek24@hotmail.co.uk>
2012-01-06 18:19:47 +0000

    added most sfText functions

--- sfml.scm
+++ sfml.scm
@@ -191,7 +191,44 @@
           
           ;; sfText ---
               
-;              sf-text-create
+              sf-text-create
+              sf-text-copy
+              sf-text-destroy
+              sf-text-set-x
+              sf-text-set-y
+              sf-text-set-position
+              sf-text-set-scale-x
+              sf-text-set-scale-y
+              sf-text-set-scale
+              sf-text-set-rotation
+              sf-text-set-origin
+              sf-text-set-color
+              ;sf-text-set-blend-mode
+              sf-text-get-x
+              sf-text-get-y
+              sf-text-get-scale-x
+              sf-text-get-scale-y
+              sf-text-get-rotation
+              sf-text-get-origin-x
+              sf-text-get-origin-y
+              ;sf-text-get-color
+              sf-text-move
+              sf-text-scale
+              sf-text-rotate
+              sf-text-transform-to-local
+              sf-text-transform-to-global
+              sf-text-set-string
+              ;sf-text-set-unicode-string
+              sf-text-set-font
+              sf-text-set-character-size
+              ;sf-text-set-style
+              ;sf-text-get-unicode-string
+              sf-text-get-string
+              sf-text-get-font
+              sf-text-get-character-size
+              sf-text-get-style
+              sf-text-get-character-pos
+              ;sf-text-get-rect
 
           ;; etc.
 
@@ -221,13 +258,15 @@ EOF
 (define-foreign-type sfKeyCode "sfKeyCode" '())
 (define-foreign-type sfView "sfView" '())
 (define-foreign-type sfFont "sfFont" '())
+(define-foreign-type sfText "sfText" '())
 
 (define sf-default-style
   (foreign-value "sfDefaultStyle" unsigned-integer64))
 
 (define sf-render-window-create
   (foreign-lambda* (c-pointer sfRenderWindow)
-    ((c-string title) (int width) (int height) (unsigned-integer64 style) ((c-pointer sfContextSettings) ctxs))
+    ((c-string title) (int width) (int height)
+     (unsigned-integer64 style) ((c-pointer sfContextSettings) ctxs))
     "sfVideoMode mode = { width, height, 32 };
      C_return(sfRenderWindow_Create(mode, title, style, ctxs));"))
 
@@ -238,12 +277,14 @@ EOF
 (define sf-render-window-clear
   (let ((c-win-clr
           (foreign-lambda* void
-            (((c-pointer sfRenderWindow) window) (int r) (int g) (int b) (int a))
+            (((c-pointer sfRenderWindow) window) (int r) (int g) 
+                                                 (int b) (int a))
             "sfColor clr = { r, g, b, a };
              sfRenderWindow_Clear(window, clr);")))
     (lambda (window cvec)
       (c-win-clr window
-                 (vector-ref cvec 0) (vector-ref cvec 1) (vector-ref cvec 2) (vector-ref cvec 3)))))
+                 (vector-ref cvec 0) (vector-ref cvec 1)
+                 (vector-ref cvec 2) (vector-ref cvec 3)))))
 
 (define sf-render-window-draw-sprite
   (foreign-lambda void
@@ -282,7 +323,8 @@ EOF
 (define sf-render-window-wait-event
   (let ((c-rw-wait-evt
           (foreign-lambda int
-            "sfRenderWindow_WaitEvent" (c-pointer sfRenderWindow) (c-pointer sfEvent))))
+            "sfRenderWindow_WaitEvent"
+            (c-pointer sfRenderWindow) (c-pointer sfEvent))))
     (lambda (window event)
       (not (zero? (c-rw-wait-evt window event))))))
 
@@ -300,7 +342,8 @@ EOF
 
 (define sf-render-window-set-size
   (foreign-lambda void
-    "sfRenderWindow_SetSize" (c-pointer sfRenderWindow) unsigned-integer unsigned-integer))
+    "sfRenderWindow_SetSize"
+    (c-pointer sfRenderWindow) unsigned-integer unsigned-integer))
 
 (define sf-render-window-set-title
   (foreign-lambda void
@@ -328,7 +371,8 @@ EOF
 
 (define sf-render-window-set-framerate-limit
   (foreign-lambda void
-    "sfRenderWindow_SetFramerateLimit" (c-pointer sfRenderWindow) unsigned-integer))
+    "sfRenderWindow_SetFramerateLimit"
+    (c-pointer sfRenderWindow) unsigned-integer))
 
 (define sf-render-window-get-frame-time
   (foreign-lambda unsigned-integer
@@ -492,7 +536,8 @@ EOF
             "sfColor clr = { r, g, b, a };
              sfSprite_SetColor(sprite, clr);")))
     (lambda (spr cvec)
-      (c-set-clr spr (vector-ref cvec 0) (vector-ref cvec 1) (vector-ref cvec 2) (vector-ref cvec 3)))))
+      (c-set-clr spr (vector-ref cvec 0) (vector-ref cvec 1)
+                     (vector-ref cvec 2) (vector-ref cvec 3)))))
 
 (define sf-sprite-get-scale-x
   (foreign-lambda
@@ -532,11 +577,13 @@ EOF
 
 (define sf-sprite-transform-to-local
   (foreign-lambda
-    void "sfSprite_TransformToLocal" (c-pointer sfSprite) float float (c-pointer float) (c-pointer float)))
+    void "sfSprite_TransformToLocal"
+    (c-pointer sfSprite) float float (c-pointer float) (c-pointer float)))
 
 (define sf-sprite-transform-to-global
   (foreign-lambda
-    void "sfSprite_TransformToGlobal" (c-pointer sfSprite) float float (c-pointer float) (c-pointer float)))
+    void "sfSprite_TransformToGlobal"
+    (c-pointer sfSprite) float float (c-pointer float) (c-pointer float)))
 
 (define (sf-event-create)
   ((foreign-lambda* (c-pointer sfEvent) ((int a)) ; HAR HAR DUMMIES
@@ -705,7 +752,7 @@ EOF
              sfView_SetViewport(view, fr);")))
     (lambda (view frect)
       (c-view-setvp view (vector-ref frect 0) (vector-ref frect 1)
-                      (vector-ref frect 2) (vector-ref frect 3)))))
+                         (vector-ref frect 2) (vector-ref frect 3)))))
 
 (define sf-view-reset
   (let ((c-view-reset
@@ -716,7 +763,7 @@ EOF
              sfView_Reset(view, fr);")))
     (lambda (view frect)
       (c-view-reset view (vector-ref frect 0) (vector-ref frect 1)
-                      (vector-ref frect 2) (vector-ref frect 3)))))
+                         (vector-ref frect 2) (vector-ref frect 3)))))
 
 (define sf-view-get-center-x
   (foreign-lambda float
@@ -778,6 +825,156 @@ EOF
 (define sf-font-get-default-font
   (foreign-lambda (c-pointer sfFont)
     "sfFont_GetDefaultFont"))
+
+(define sf-text-create
+  (foreign-lambda (c-pointer sfText)
+    "sfText_Create"))
+
+(define sf-text-copy
+  (foreign-lambda (c-pointer sfText)
+    "sfText_Copy" (c-pointer sfText)))
+
+(define sf-text-destroy
+  (foreign-lambda void
+    "sfText_Destroy" (c-pointer sfText)))
+
+(define sf-text-set-x
+  (foreign-lambda void
+    "sfText_SetX" (c-pointer sfText) float))
+
+(define sf-text-set-y
+  (foreign-lambda void
+    "sfText_SetY" (c-pointer sfText) float))
+
+(define sf-text-set-position
+  (foreign-lambda void
+    "sfText_SetPosition" (c-pointer sfText) float float))
+
+(define sf-text-set-scale-x
+  (foreign-lambda void
+    "sfText_SetScaleX" (c-pointer sfText) float))
+
+(define sf-text-set-scale-y
+  (foreign-lambda void
+    "sfText_SetScaleY" (c-pointer sfText) float))
+
+(define sf-text-set-scale
+  (foreign-lambda void
+    "sfText_SetScale" (c-pointer sfText) float float))
+
+(define sf-text-set-rotation
+  (foreign-lambda void
+    "sfText_SetRotation" (c-pointer sfText) float))
+
+(define sf-text-set-origin
+  (foreign-lambda void
+    "sfText_SetOrigin" (c-pointer sfText) float float))
+
+(define (sf-text-set-color spr cvec)
+  (let ((c-set-tclr
+          (foreign-lambda* void
+            (((c-pointer sfText) sprite) (int r) (int g) (int b) (int a))
+            "sfColor clr = { r, g, b, a };
+             sfText_SetColor(sprite, clr);")))
+    (lambda (spr cvec)
+      (c-set-tclr spr (vector-ref cvec 0) (vector-ref cvec 1)
+                      (vector-ref cvec 2) (vector-ref cvec 3)))))
+
+;sf-text-set-blend-mode
+
+(define sf-text-get-x
+  (foreign-lambda float
+    "sfText_GetX" (c-pointer sfText)))
+
+(define sf-text-get-y
+  (foreign-lambda float
+    "sfText_GetY" (c-pointer sfText)))
+
+(define sf-text-get-scale-x
+  (foreign-lambda float
+    "sfText_GetScaleX" (c-pointer sfText)))
+
+(define sf-text-get-scale-y
+  (foreign-lambda float
+    "sfText_GetScaleY" (c-pointer sfText)))
+
+(define sf-text-get-rotation
+  (foreign-lambda float
+    "sfText_GetRotation" (c-pointer sfText)))
+
+(define sf-text-get-origin-x
+  (foreign-lambda float
+    "sfText_GetOriginX" (c-pointer sfText)))
+
+(define sf-text-get-origin-y
+  (foreign-lambda float
+    "sfText_GetOriginY" (c-pointer sfText)))
+
+;sf-text-get-color
+
+(define sf-text-move
+  (foreign-lambda void
+    "sfText_Move" (c-pointer sfText) float float))
+
+(define sf-text-scale
+  (foreign-lambda void
+    "sfText_Scale" (c-pointer sfText) float float))
+
+(define sf-text-rotate
+  (foreign-lambda void
+    "sfText_Rotate" (c-pointer sfText) float))
+
+(define sf-text-transform-to-local
+  (foreign-lambda
+    void "sfText_TransformToLocal"
+    (c-pointer sfText) float float (c-pointer float) (c-pointer float)))
+
+(define sf-text-transform-to-global
+  (foreign-lambda
+    void "sfText_TransformToGlobal"
+    (c-pointer sfText) float float (c-pointer float) (c-pointer float)))
+
+(define sf-text-set-string
+  (foreign-lambda void
+    "sfText_SetString" (c-pointer sfText) c-string))
+
+;sf-text-set-unicode-string
+
+(define sf-text-set-font
+  (foreign-lambda void
+    "sfText_SetFont" (c-pointer sfText) (c-pointer sfFont)))
+
+(define sf-text-set-character-size
+  (foreign-lambda void
+    "sfText_SetCharacterSize" (c-pointer sfText) unsigned-integer))
+
+;sf-text-set-style
+
+;sf-text-get-unicode-string
+
+(define sf-text-get-string
+  (foreign-lambda c-string
+    "sfText_GetString" (c-pointer sfText)))
+
+(define sf-text-get-font
+  (foreign-lambda (c-pointer sfFont)
+    "sfText_GetFont" (c-pointer sfText)))
+
+(define sf-text-get-character-size
+  (foreign-lambda unsigned-integer
+    "sfText_GetCharacterSize" (c-pointer sfText)))
+
+(define sf-text-get-style
+  (foreign-lambda unsigned-integer32
+    "sfText_GetStyle" (c-pointer sfText)))
+
+(define sf-text-get-character-pos
+  (foreign-lambda void
+    "sfText_GetCharacterPos"
+    (c-pointer sfText) unsigned-integer (c-pointer float) (c-pointer float)))
+
+;sf-text-get-rect
+
 
 (define sf-clock-create
   (foreign-lambda
